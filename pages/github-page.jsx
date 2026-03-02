80a4a4c3989eb80c5a2279e261ed9e5c9c86e875
Philip Quattrochi <philip.quattrochi@gmail.com>
2026-03-02 17:44:09 -0500

    pages

---
+++ pages/github-page.jsx
@@ -0,0 +1,13 @@
+export default function ProjectGithubPage() {
+  return (
+    <main>
+      <h1>GitHub Page Content</h1>
+      <h2>Project Team and Catchy Product Image</h2>
+      
+      <h2>Brief Introduction to Your Team Project</h2>
+      <h2>Summary of Your User Research</h2>
+      <h2>Storyboard Illustrating Three Different Levels of Tasks</h2>
+      <h2>Links to Mockup and Interactivity Prototype</h2>
+    </main>
+  );
+}
